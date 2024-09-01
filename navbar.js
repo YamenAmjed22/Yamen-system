fc48c90fbb3cef8c36baa88fe28f3760f4008713
Yamen Amjed <yamenamjed2022@gmail.com>
2024-09-01 09:06:25 -0700

    first commit

---
+++ navbar.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-container').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+});
